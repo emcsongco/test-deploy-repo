d8188514555055814787e2e25fb486d27c162965
Eva S <evamarie.songco@gmail.com>
2022-12-05 06:57:54 -0800

    Added @RestController to main and two @GetMappings

--- src/main/java/com/example/testdeploy/TestDeployApplication.java
+++ src/main/java/com/example/testdeploy/TestDeployApplication.java
@@ -2,17 +2,29 @@ package com.example.testdeploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class TestDeployApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestDeployApplication.class, args);
 	}
 
+	@GetMapping("/welcome")
+	public String appWelcome() {
+		return "Welcome to my API.";
+	}
+
+	@GetMapping("/welcome")
+	public List<String> showNames() {
+		return List.of("Sam","Steph","Ollie");
+	}
+
 }
 
-//challenge
-//		make a new public repo in gh
-//		link it to this test-repo project
-//		push all files to github
+
