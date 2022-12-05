948f3ca9b1de695cddac2bd1f983c4166de83db5
Eva S <evamarie.songco@gmail.com>
2022-12-05 06:49:46 -0800

    initial commit and repo created

---
+++ src/main/java/com/example/testdeploy/TestDeployApplication.java
@@ -0,0 +1,18 @@
+package com.example.testdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeployApplication.class, args);
+	}
+
+}
+
+//challenge
+//		make a new public repo in gh
+//		link it to this test-repo project
+//		push all files to github
